6165896bdf7de42b385fab512e248760c917956e
유주현 <yujoo@synapsoft.co.kr>
2018-04-14 01:12:56 +0900

    add scss in webpack

--- TestWebpack/Test/src/js/entry.js
+++ TestWebpack/Test/src/js/entry.js
@@ -3,7 +3,11 @@
 // var world = require('./world');
 // document.write(hello + ',' + world + '!');
 
-import '../css/common.css';
-import hello from './hello';
-import world from './world';
-document.write(`${hello}, ${world}!`);
+require('../scss/common.scss')
+
+import Utils from './Utils'
+import hello from './hello'
+import world from './world'
+
+// document.write(`${hello}, ${world}!`)
+Utils.log(`${hello}, ${world}!`)
--- TestWebpack/Test/webpack.config.js
+++ TestWebpack/Test/webpack.config.js
@@ -1,33 +1,51 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
-    entry: {
-      'entry': './src/js/entry.js'
-    },
-    output: {
-      filename: './dist/bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [[
-                        'env', {
-                            targets: {
-                                browsers: ['last 2 versions']
-                            }
-                        }
-                    ]]
-                }
-            }
-        ]
-    }
-  };
+	entry: {
+		main: './src/js/entry.js'
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.join(__dirname, './dist')
+	},
+	module: {
+		rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+						// presets: [[
+						//     'env', {
+						//         targets: {
+						//             browsers: ['last 2 versions']
+						//         }
+						//     }
+						// ]]
+					}
+				}
+			},
+			// {
+			//     test: /\.css$/,
+			//     use: [
+			//         'style-loader',
+			//         'css-loader'
+			//     ]
+			// },
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			},
+		]
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin(),
+		new ExtractTextPlugin('style.css')
+	]
+};
